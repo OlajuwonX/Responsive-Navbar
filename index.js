2d93898be5811c949a7d5c1f38c1530d3637abb3
OlajuwonX <olasimboolajuwon@gmail.com>
2024-08-13 12:10:34 +0100

    lattest update

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function showSideBar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'flex';
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
 }
 
 function hideSideBar() {
-    const sidebar = document.querySelector('.sidebar');
-    sidebar.style.display = 'none';
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
 }
+
+function generateRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const images = [
+  './public/Cool_wallpaper1.jpg',
+  './public/Cool_wallpaper2.jpg',
+  './public/Cool_wallpaper3.jpg',
+  './public/Cool_wallpaper4.jpg'
+];
+
+const randomIndex = generateRandomInt(images.length);
+const selectedBackground = images[randomIndex];
+
+document.body.style.backgroundImage = `url('${selectedBackground}')`;
